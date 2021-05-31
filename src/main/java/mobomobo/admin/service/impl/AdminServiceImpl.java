8dd92eae893e86e0e3d379a5b10a93beb270d95e
choiSB <choi13698@naver.com>
2021-05-31 17:05:45 +0900

    오류 수정후 재 commit

--- src/main/java/mobomobo/admin/service/impl/AdminServiceImpl.java
+++ src/main/java/mobomobo/admin/service/impl/AdminServiceImpl.java
@@ -3,8 +3,15 @@ package mobomobo.admin.service.impl;
 import org.springframework.stereotype.Service;
 
 import mobomobo.admin.service.face.AdminService;
+import mobomobo.admin.util.Paging;
 
 @Service
 public class AdminServiceImpl implements AdminService{
+
+	@Override
+	public Paging getPaging(Paging userPaging) {
+		
+		return null;
+	}
 
 }
